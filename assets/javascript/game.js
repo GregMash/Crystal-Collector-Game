1e32a098c6abb0b5f5ed2f112ded8ddc3124c91a
Link <username@users.noreply.github.com>
2018-11-06 23:17:21 -0700

    each crystal separated number, not DRY tho

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -6,14 +6,64 @@ if their score adds up to the target number incrememnt wins  -reset
 if they go over the value, increment losses - reset
 */
 
-var targetNum = Math.floor(Math.random() * 112) + 19;
-Math.floor(Math.random() * 100) + 1;
+
+
+var targetNum = Math.floor(Math.random() * 120) + 19;
 console.log(targetNum);
+$("#target-number").text(targetNum);
 
 var counter = 0;
 
+var number1 = Math.floor(Math.random() * 12 + 1);
+console.log(number1);
+var number2 = Math.floor(Math.random() * 12 + 1);
+console.log(number2);
+var number3 = Math.floor(Math.random() * 12 + 1);
+console.log(number3);
+var number4 = Math.floor(Math.random() * 12 + 1);
+console.log(number4);
 
-$(".crystal").on("click", function () {
-    counter += 1;
-console.log('you clicked a crystal ' + counter + " times")
+
+$("#1").on("click", function () {
+    counter += number1;
+    console.log('you clicked a crystal ' + counter + " times");
+
+    if (counter === targetNum) {
+        console.log("winner winner");
+    } else if (counter > targetNum) {
+        console.log('LOSER');
+    }
+});
+
+$("#2").on("click", function () {
+    counter += number2;
+    console.log('you clicked a crystal ' + counter + " times");
+
+    if (counter === targetNum) {
+        console.log("winner winner");
+    } else if (counter > targetNum) {
+        console.log('LOSER');
+    }
+});
+
+$("#3").on("click", function () {
+    counter += number3;
+    console.log('you clicked a crystal ' + counter + " times");
+
+    if (counter === targetNum) {
+        console.log("winner winner");
+    } else if (counter > targetNum) {
+        console.log('LOSER');
+    }
+});
+
+$("#4").on("click", function () {
+    counter += number4;
+    console.log('you clicked a crystal ' + counter + " times");
+
+    if (counter === targetNum) {
+        console.log("winner winner");
+    } else if (counter > targetNum) {
+        console.log('LOSER');
+    }
 });
