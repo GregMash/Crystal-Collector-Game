476bb8b5c0ccda1f6b785443b9959a595c3e6d60
Link <username@users.noreply.github.com>
2018-11-08 20:30:33 -0700

    game update

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,16 +1,11 @@
 /*
+Pseudocode
 generate a random target number to guess up to - 19-120
 add 4 images of crystals and assign them a random number value -1 -12
 make the user click on the images to increase their score
 if their score adds up to the target number incrememnt wins  -reset
 if they go over the value, increment losses - reset
 */
-
-
-//This sets the target number for the user to guess up to
-var targetNum = Math.floor(Math.random() * 102) + 19;
-console.log(targetNum);
-$("#target-number").text(targetNum);
 
 //This starts the user score at 0
 var counter = 0;
@@ -24,6 +19,12 @@ $('#wins').text(wins);
 var losses = 0;
 $('#losses').text(losses);
 
+//This sets the target number for the user to guess up to
+var targetNum = Math.floor(Math.random() * 102) + 19;
+console.log(targetNum);
+$("#target-number").text(targetNum);
+
+
 //This function generates a random number for the crystal value between 1 and 12
 function numGenerate() {
     var crystalNumber = Math.floor(Math.random() * 12 + 1);
@@ -36,10 +37,27 @@ $("#crystal2").attr("data-value", numGenerate());
 $("#crystal3").attr("data-value", numGenerate());
 $("#crystal4").attr("data-value", numGenerate());
 
+
+
 //This is our reset function
+function reset () {
+
+    //This starts the user score at 0
+counter = 0;
+//$("#score").text(counter);
+targetNum = Math.floor(Math.random() * 102) + 19;
+$("#target-number").text(targetNum);
 
 
 
+//This gives each crystal in the HTML an attribute of data-value, and a value of a random number from the numGenerate function
+$("#crystal1").attr("data-value", numGenerate());
+$("#crystal2").attr("data-value", numGenerate());
+$("#crystal3").attr("data-value", numGenerate());
+$("#crystal4").attr("data-value", numGenerate());
+
+
+}
 
 //This is our click event
 $(".crystal").on("click", function () {
@@ -55,10 +73,14 @@ $(".crystal").on("click", function () {
     if (counter === targetNum) {
         wins++;
         $('#wins').text(wins);
+        reset ();
 
     } else if (counter > targetNum) {
         losses++;
         $('#losses').text(losses);
-
+        reset ();
     }
 });
+
+
+
